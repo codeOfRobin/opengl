e48d3d2091e4346fa9260ea10f6a2dc30a2b2359
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-07-31 21:24:28 +0530

    restarted tutorial

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -12,71 +12,40 @@
 
 void display(void);
 void reshape(int, int);
+void windowInit();
+void renderScene();
+
 int main(int argc, char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
     glutInit(&argc, argv);
-    glutInitWindowSize(512, 512);
-    
-    glutInitWindowPosition(100, 300);
-#warning check out what happens when you tru GLUT_Single
-    glutInitDisplayMode(GLUT_RGBA|GLUT_DEPTH|GLUT_DOUBLE);
-    glutCreateWindow("trial");
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glClearColor(0.4, 0.1, 1, 0.5);
+    windowInit();
+    glutDisplayFunc(renderScene);
     glutMainLoop();
     return 0;
+
 }
 
 
-void display()
+
+
+void windowInit()
 {
-    /* clear the color buffer (resets everything to black) */
-    glClear(GL_COLOR_BUFFER_BIT);
-    
-    /* set the current drawing color to red */
-    glColor3f(1, 0, 0);
-    
-    /* start drawing triangles, each triangle takes 3 vertices */
+    glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH);
+
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(512, 512);
+    glutCreateWindow("trial");
+
+}
+
+
+void renderScene()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glBegin(GL_TRIANGLES);
-    
-    /* give the 3 triangle vertex coordinates 1 at a time */
-    glVertex2f(10, 10);
-    glVertex2f(250, 400);
-    glVertex2f(400, 10);
-    
-    /* tell OpenGL we're done drawing triangles */
+    glVertex3f(-0.5,-0.5,0.0);
+    glVertex3f(0.5,0.0,0.0);
+    glVertex3f(0.0,0.5,0.0);
     glEnd();
-    
-    /* swap the back and front buffers so we can see what we just drew */
     glutSwapBuffers();
 }
-
-/*! glut reshape callback function.  GLUT calls this function whenever
- the window is resized, including the first time it is created.
- You can use variables to keep track the current window size.
- */
-void reshape(int width, int height)
-{
-    /* tell OpenGL we want to display in a recangle that is the
-     same size as the window
-     */
-    glViewport(0,0,width/2,height/2);
-    
-    /* switch to the projection matrix */
-    glMatrixMode(GL_PROJECTION);
-    
-    /* clear the projection matrix */
-    glLoadIdentity();
-    
-    /* set the camera view, orthographic projection in 2D */
-    gluOrtho2D(0,width,0,height);
-    
-    /* switch back to the model view matrix */
-    glMatrixMode(GL_MODELVIEW);
-}
-
-
