218152f799a8bc1408059d1744691d6da0fb419e
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-08-01 00:02:58 +0530

    understood quite a few more things. This might get interesting.

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -14,19 +14,42 @@ void display(void);
 void reshape(int, int);
 void windowInit();
 void renderScene();
-
+void reshape();
 int main(int argc, char * argv[])
 {
     glutInit(&argc, argv);
     windowInit();
     glutDisplayFunc(renderScene);
+    glutReshapeFunc(reshape);
     glutMainLoop();
     return 0;
 
 }
 
 
-
+void reshape(int w, int h)
+{
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window of zero width).
+	if(h == 0)
+		h = 1;
+	float ratio = 1.0* w / h;
+    
+	// Use the Projection Matrix
+	glMatrixMode(GL_PROJECTION);
+    
+    // Reset Matrix
+	glLoadIdentity();
+    
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
+    
+	// Set the correct perspective.
+	gluPerspective(45,ratio,1,1000);
+    
+	// Get Back to the Modelview
+	glMatrixMode(GL_MODELVIEW);
+}
 
 void windowInit()
 {
@@ -43,9 +66,9 @@ void renderScene()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glBegin(GL_TRIANGLES);
-    glVertex3f(-0.5,-0.5,0.0);
-    glVertex3f(0.5,0.0,0.0);
-    glVertex3f(0.0,0.5,0.0);
+    glVertex3f(-2,-2,-5.0);
+    glVertex3f(2,0.0,-5.0);
+    glVertex3f(0.0,2,-5.0);
     glEnd();
     glutSwapBuffers();
 }
