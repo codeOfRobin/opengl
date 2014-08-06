07c05c56f02e7f6ceb1b6dab930f8b3a11f7081b
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-08-07 01:26:47 +0530

    added clear color So, I can change backgrounds later
    Also, for reading, I’m on  http://www.glprogramming.com/red/chapter02.html
    Example 2-4

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -123,6 +123,8 @@ void renderScene()
 {
     
    	// Clear Color and Depth Buffers
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClearDepth(1.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glutIgnoreKeyRepeat(false);
 	// Reset transformations
